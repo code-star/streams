fe308b15bf9f0a07a5ff6a0367eb1dedd0c4e418
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-04 15:27:02 +0100

    Move pages to folders

--- packages/client/src/components/pages/ReactLeafletPage/index.js
+++ packages/client/src/components/pages/ReactLeafletPage/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const ReactLeafletPage = () => {
   return (
     <div>
       <h2>React Leaflet Page</h2>
       <div>
-        Under construction 🙂
         <Link to="/">Back to Home Page</Link>
+      </div>
+      <div>
+        test
       </div>
     </div>
   )
