0c5e79286b9c07ae49f7b1c527eff14d4e7fa5c7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-10 15:31:47 +0100

    Refine marker styles

--- packages/client/src/components/patterns/atoms/map/Marker/Marker.js
+++ packages/client/src/components/patterns/atoms/map/Marker/Marker.js
@@ -15,7 +15,7 @@ const Marker = ({ text, active, label }) => (
     <div className={cx('marker', { 'marker--active': active })} style={markerPosition}>
         <svg xmlns="http://www.w3.org/2000/svg" width="44" height="55" viewBox="0 0 44 55">
             <g fill="none" fillRule="evenodd">
-                {label ? <path className="marker__body marker__body--ema" stroke="#FFF" strokeWidth="3" d="M22 53s20-18.758 20-31.362S33.046 2 22 2 2 9.034 2 21.638C2 34.242 22 53 22 53z" />
+                {label ? <path className="marker__body" stroke="#FFF" strokeWidth="3" d="M22 53s20-18.758 20-31.362S33.046 2 22 2 2 9.034 2 21.638C2 34.242 22 53 22 53z" />
                 : <path className="marker__body" stroke="#FFF" strokeWidth="3" d="M22 53s20-18.758 20-31.362S33.046 2 22 2 2 9.034 2 21.638C2 34.242 22 53 22 53z" />}
                 {label && <path fill="#FFF" fillRule="nonzero" d="M21.91 13l2.636 4.663 5.274 1.053-3.644 3.935.623 5.314-4.889-2.231-4.889 2.23.623-5.313L14 18.716l5.274-1.053z" />}
             </g>
