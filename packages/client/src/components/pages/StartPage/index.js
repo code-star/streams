090c1c57a58ee6c53a7bcb084cb9dc01aa5a5f91
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-10 15:34:59 +0100

    Remove other links

--- packages/client/src/components/pages/StartPage/index.js
+++ packages/client/src/components/pages/StartPage/index.js
@@ -8,12 +8,14 @@ const HomePage = () => {
         <li>
           <Link to="/google-map-react">GoogleMapReact</Link>
         </li>
-        <li>
-          <Link to="/react-google-maps">ReactGoogleMaps</Link>
-        </li>
-        <li>
-          <Link to="/react-leaflet">ReactLeaflet</Link>
-        </li>
+        {/*
+          <li>
+            <Link to="/react-google-maps">ReactGoogleMaps</Link>
+          </li>
+          <li>
+            <Link to="/react-leaflet">ReactLeaflet</Link>
+          </li>
+        */}
       </ul>
     </div>
   )
