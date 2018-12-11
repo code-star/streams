0a52cea026e328d639da084122e0635c01999d4e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-11 16:56:21 +0100

    Adjust listen message

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -17,5 +17,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+  console.log(`Codestar Streams Server listening on port ${process.env.PORT}!`),
 )
