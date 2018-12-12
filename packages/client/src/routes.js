85dfa485da2c61a4627f31b8a69b2a09fcd963c7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-12 11:32:59 +0100

    Restore route

--- packages/client/src/routes.js
+++ packages/client/src/routes.js
@@ -1,2 +1,2 @@
-export const startPageRoute = process.env.BASE_URL ? `${process.env.BASE_URL}/`: '/streams'
+export const startPageRoute = process.env.BASE_URL ? `${process.env.BASE_URL}/`: '/'
 export const demoPageRoute = process.env.BASE_URL ? `${process.env.BASE_URL}/demo`: '/demo'
