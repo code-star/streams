91056dfe2548ff75c6f9c463be781acde3546014
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-12 13:49:25 +0100

    Refactor start and demo pages

--- packages/client/src/pages/DemoPage/index.js
+++ packages/client/src/pages/DemoPage/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Map from './Map'
+import Map from '../../patterns/molecules/map/Map'
 import { startPageRoute } from '../../routes'
+import Header from '../../patterns/molecules/navigation/Header'
 
 const DemoReactPage = () => {
   return (
     <div>
-      <Link to={startPageRoute}>Back to Start Page</Link>
-      <Map />
+      <Header>Codestar Streams</Header>
+       <div className="d-flex flex-wrap flex-lg-nowrap">
+          <div className="width-full">
+            <div className="border p-3">
+                <Link to={startPageRoute}>
+                  <button className="btn-link" type="button">Back to Start Page</button>
+                </Link>
+            </div>
+            <div className="border p-3">
+                <Map />
+            </div>
+          </div>
+       </div>
     </div>
   )
 }
