6505a59e3dce518b70cf51d9761593cd24e5b707
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-12 13:48:44 +0100

    Add navigation molecules

---
+++ packages/client/src/patterns/molecules/navigation/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { string, node } from 'prop-types'
+
+const style = {
+  'position': '-webkit-sticky',
+  'position': 'sticky',
+  'top': 0,
+  'left': 0,
+  'right': 0,
+  'zIndex': 101
+}
+
+const Header = ({ className, children }) => (
+  <div className="bg-gray-dark" style={style}>
+    <div className="main-nav d-flex flex-justify-between px-3 pl-md-4 pr-md-4 py-3 box-shadow bg-gray-dark Details js-details-container">
+       <div className="d-flex flex-self-center flex-lg-auto mr-lg-2">
+          <div className="flex-self-center Details-content--shown">
+             <div className="dropdown js-menu-container js-select-menu">
+                <h1 className="h4 text-normal">
+                   <button className="btn-link text-white link-white no-underline js-menu-target" type="button" aria-haspopup="true" aria-expanded="false">
+                      {children}
+                   </button>
+                </h1>
+             </div>
+          </div>
+       </div>
+    </div>
+  </div>
+)
+
+Header.defaultProps = {
+  className: '',
+  children: null,
+}
+
+Header.propTypes = {
+  className: string,
+  children: node,
+}
+
+export default Header
