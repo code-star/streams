8356ab9b9df5dc2865be2de1884cb2d71a0134d6
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-12 15:00:29 +0100

    Adjust test

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.send({ 'test': '456' })
+  res.send({ 'test': '123' })
 })
 
 const port = process.env.PORT || 3000
