8285edb1f7e1666a90fa4303311a162817c4d1e8
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-13 11:29:58 +0100

    Add apollo server

---
+++ packages/server/src/datasources/station.js
@@ -0,0 +1,33 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+const fetch = require("node-fetch");
+const { stations } = require('./stations.json');
+
+class StationAPI extends RESTDataSource {
+  async getAllStations() {
+    return stations && stations.length ? stations.map(l => this.stationReducer(l)) : []
+  }
+
+  stationReducer(station) {
+    return {
+      id: station.id,
+      lat: station.lat,
+      lng: station.lng,
+      text: station.text,
+      isFocussed: station.isFocussed,
+      isSelected: station.isSelected
+    }
+  }
+
+  async getStationById({ stationId }) {
+    const res = await this.get('stations', { id: stationId })
+    return this.stationReducer(res[0])
+  }
+
+  getStationsByIds({ stationIds }) {
+    return Promise.all(
+      stationIds.map(stationId => this.getStationById({ stationId })),
+    )
+  }
+}
+
+module.exports = StationAPI
---
+++ packages/server/src/resolvers.js
@@ -0,0 +1,25 @@
+const { paginateResults } = require('./utils');
+
+module.exports = {
+  Query: {
+    stations: async (_, { pageSize = 20, after }, { dataSources }) => {
+      const allStations = await dataSources.stationAPI.getAllStations()
+      allStations.reverse()
+
+      const stations = paginateResults({
+        after,
+        pageSize,
+        results: allStations,
+      })
+
+      return {
+        stations,
+        cursor: stations.length ? stations[stations.length - 1].cursor : null,
+        hasMore: stations.length
+          ? stations[stations.length - 1].cursor !==
+            allStations[allStations.length - 1].cursor
+          : false,
+      }
+    }
+  }
+}
