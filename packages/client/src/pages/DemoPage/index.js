09208f742c117bcf588e284006bad7ff0709f76d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-13 17:06:04 +0100

    Lint

--- packages/client/src/pages/DemoPage/index.js
+++ packages/client/src/pages/DemoPage/index.js
@@ -4,24 +4,22 @@ import Map from '../../patterns/molecules/map/Map'
 import { startPageRoute } from '../../routes'
 import Header from '../../patterns/molecules/navigation/Header'
 
-const DemoReactPage = () => {
-  return (
-    <div>
-      <Header>Codestar Streams</Header>
-       <div className="d-flex flex-wrap flex-lg-nowrap">
-          <div className="width-full">
-            <div className="border p-3">
-                <Link to={startPageRoute}>
-                  <button className="btn-link" type="button">Back to Start Page</button>
-                </Link>
-            </div>
-            <div className="border p-3">
-                <Map />
-            </div>
+const DemoReactPage = () => (
+  <div>
+    <Header>Codestar Streams</Header>
+     <div className="d-flex flex-wrap flex-lg-nowrap">
+        <div className="width-full">
+          <div className="border p-3">
+              <Link to={startPageRoute}>
+                <button className="btn-link" type="button">Back to Start Page</button>
+              </Link>
           </div>
-       </div>
-    </div>
-  )
-}
+          <div className="border p-3">
+              <Map />
+          </div>
+        </div>
+     </div>
+  </div>
+)
 
 export default DemoReactPage
