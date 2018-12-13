2395919c4c09d004224398dcb2446c91bfa60a79
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-13 17:06:43 +0100

    Collect statios

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,30 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import StartPage from './pages/StartPage';
-import DemoPage from './pages/DemoPage';
+/* eslint no-console: 0 */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import gql from "graphql-tag"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import StartPage from './pages/StartPage'
+import DemoPage from './pages/DemoPage'
 import { startPageRoute, demoPageRoute } from './routes'
 import './index.scss'
 
 function addDivWithIdToBody() {
-  const appElement = document.createElement("div");
-  appElement.id = 'app';
-  document.body.appendChild(appElement);
+  const appElement = document.createElement("div")
+  appElement.id = 'app'
+  document.body.appendChild(appElement)
 }
 
 addDivWithIdToBody()
 
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path={startPageRoute} component={StartPage} />
-        <Route exact path={demoPageRoute} component={DemoPage} />
-      </Switch>
-    </Router>
-  );
-}
+const cache = new InMemoryCache()
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'https://streams-server.herokuapp.com/graphql',
+  }),
+})
 
-module.hot.accept();
+client
+  .query({
+    query: gql`
+      query GetStations {
+        stations(pageSize: 3) {
+          stations {
+            id
+            text
+          }
+        }
+      }
+    `,
+  })
+  .then(result => console.log('~result~', result))
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path={startPageRoute} component={StartPage} />
+      <Route exact path={demoPageRoute} component={DemoPage} />
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'))
+
+module.hot.accept()
